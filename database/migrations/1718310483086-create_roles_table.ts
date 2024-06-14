60829c7801f85c17c45b93c61e44d79febae6195
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2024-06-13 19:29:11 -0600

    DB initial connection

---
+++ database/migrations/1718310483086-create_roles_table.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table, TableColumn } from "typeorm";
+
+export class CreateRolesTable1718310483086 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'Roles',
+            columns: [
+                new TableColumn({
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isNullable: false,
+                    isGenerated: true,       
+                    generationStrategy: 'increment',
+                }),
+                new TableColumn({
+                    name: 'role',
+                    type: 'varchar',
+                    length: '15',
+                    isNullable: false,
+                    isUnique: true,
+                }),
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('Roles');
+    }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from 'database/config/data-source';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => AppDataSource.options,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
