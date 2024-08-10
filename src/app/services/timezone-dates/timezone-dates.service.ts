4d672683a229acfa33f92a5f1cc8bcd8ddb7d902
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2024-08-10 03:23:39 -0600

    fix timezone

--- src/app/services/timezone-dates/timezone-dates.service.ts
+++ src/app/services/timezone-dates/timezone-dates.service.ts
@@ -6,19 +6,19 @@ export class TimezoneDatesService {
     private readonly timezone = "America/Mexico_City";
 
     convertToDate(date: string): Date {
-        return moment(date, "YYYY-MM-DD").toDate();
+        return moment.tz(date, "YYYY-MM-DD", this.timezone).toDate();
     }
 
     createDateFromDatetime(date: string): Date {
-        return moment(date).toDate();
+        return moment.tz(date, this.timezone).toDate();
     }
 
     formatStringToDateString(date: string): string {
-        return moment(date).format("DD/MM/YYYY");
+        return moment.tz(date, this.timezone).format("DD/MM/YYYY");
     }
 
     formatDateToString(date: Date): string {
-        return moment(date).format("YYYY-MM-DD");
+        return moment.tz(date, this.timezone).format("YYYY-MM-DD");
     }
 
     getCurrentDate(): Date {
@@ -26,36 +26,35 @@ export class TimezoneDatesService {
     }
 
     getDayOfWeek(date: Date): string {
-        return moment(date).format("dddd");
+        return moment.tz(date, this.timezone).format("dddd");
     }
 
     getCurrentTimeString(date: Date): string {
-        return moment(date).format("HH:mm");
+        return moment.tz(date, this.timezone).format("HH:mm");
     }
 
     getMinutesFromDate(date: Date): number {
-        return moment(date).minutes();
+        return moment.tz(date, this.timezone).minutes();
     }
 
     addDaysToDate(date: Date, days: number): Date {
-        return moment(date).add(days, "days").toDate();
+        return moment.tz(date, this.timezone).add(days, "days").toDate();
     }
 
     addRangeToDate(date: Date, hours: number, minutes: number): Date {
-        return moment(date).add(hours, "hours").add(minutes, "minutes").toDate();
+        return moment.tz(date, this.timezone).add(hours, "hours").add(minutes, "minutes").toDate();
     }
 
     setTimeToDate(date: Date, hours: number, minutes: number): Date {
-        return moment(date).set({ hours: hours, minutes: minutes, second: 0, millisecond: 0 }).toDate();
+        return moment.tz(date, this.timezone).set({ hours: hours, minutes: minutes, second: 0, millisecond: 0 }).toDate();
     }
-
     subtractTimeToDate(date: Date, hours: number, minutes: number, seconds: number, milliseconds: number): Date {
-        return moment(date).subtract(hours, "hours").subtract(minutes, "minutes").subtract(seconds, "seconds").subtract(milliseconds, "milliseconds").toDate();
+        return moment.tz(date, this.timezone).subtract(hours, "hours").subtract(minutes, "minutes").subtract(seconds, "seconds").subtract(milliseconds, "milliseconds").toDate();
     }
 
     isSameDay(first: Date, second: Date): boolean {
-        const startOfFirstDay = moment(first).startOf("day");
-        const startOfSecondDay = moment(second).startOf("day");
+        const startOfFirstDay = moment.tz(first, this.timezone).startOf("day");
+        const startOfSecondDay = moment.tz(second, this.timezone).startOf("day");
 
         return startOfFirstDay.isSame(startOfSecondDay);
     }
