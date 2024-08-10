b9b033737f6bcbd5d19b89af3079b828245911f8
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2024-08-10 02:46:55 -0600

    Update appointments.module.ts

--- src/app/modules/appointments/appointments.module.ts
+++ src/app/modules/appointments/appointments.module.ts
@@ -6,12 +6,14 @@ import { Appointment } from "@/app/entities/appointment.entity";
 import { User } from "src/app/entities/user.entity";
 import { MailerModule } from "@/app/services/mailer/mailer.module";
 import { UsersModule } from "../users/users.module";
+import { TimezoneDatesModule } from "@/app/services/timezone-dates/timezone-dates.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Appointment, User]),
         MailerModule,
         UsersModule,
+        TimezoneDatesModule,
     ],
     controllers: [AppointmentsController],
     providers: [AppointmentsService],
